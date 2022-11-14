879a1406e53d985187f62a677b28f9f88bcf8a9d
jaforeroto <112499361+jaforeroto@users.noreply.github.com>
2022-11-13 21:48:13 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -16,7 +16,7 @@ def pregunta_01():
     Carga y separación de los datos en `X` `y`
     """
      # Lea el archivo `concrete.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv("concrete.csv")
+    df = pd.read_csv("./concrete.csv")
 
     # Asigne la columna `strength` a la variable `y`.
     y = df["strength"]
